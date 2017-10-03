3e65cba5c95c45fc48af81d8fbd7f41b6f65f740
obia williams <obia.williams@gmail.com>
2017-10-03 19:34:08 +0300

    initial commit

---
+++ app/src/main/java/com/cosima/kotlin_starter_app/MainActivity.java
@@ -0,0 +1,39 @@
+package com.cosima.kotlin_starter_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+//    companion object {
+//        const val TOTAL_COUNT = "total_count"
+//    }
+
+
+    //Toast function
+    void toastMe()
+    {
+        //magic goes here
+
+    }
+    //Count Function
+    void countMe()
+    {
+        //magic goes here
+    }
+
+    //Random Number Function
+    void randomMe()
+    {
+        //magic goes here
+
+    }
+
+
+}

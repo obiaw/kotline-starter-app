0520b450cd602869a41a221d05845f0693922dd1
obia williams <obia.williams@gmail.com>
2017-10-04 18:31:28 +0300

    second activity added

---
+++ app/src/main/java/com/cosima/kotlin_starter_app/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.cosima.kotlin_starter_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    // companion object {
+//        const val TOTAL_COUNT = "total_count"
+//    }
+
+    
+    void showRandomNumber() {
+        // Get the count from the intent extras
+
+    }
+}
